3d1bbb271e047421d8cf5ae11d61d9a22ac556ed
Yoshikawa290036 <290036hirokix@gmail.com>
2022-09-16 15:40:59 +0900

    [Add] xyc

--- sh/get_pesh.sh
+++
@@ -1,3 +0,0 @@
-for ((i=2;i<=8;i+=2)); do
-    tail lowpe/pe0_${i}/tdtsh -n 1
-done
---
+++ xyc.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+dire=data
+fname=xyc
+gnuplot -e "dire = '${dire}'; fname = '${fname}'"  plt/${fname}.plt
+
+
+platex ${fname}.tex
+dvips ${fname}.dvi -o ${fname}.eps
+
+convert -density 600 -units PixelsPerInch -alpha off ${fname}.eps ${fname}.png
+
+rm ${fname}.aux
+rm ${fname}.dvi
+rm ${fname}.eps
+rm ${fname}.log
+rm ${fname}.tex
+rm ${fname}-inc.eps
+
+# convert ${fname}.png -trim ${fname}.png
+
+mv ${fname}.png  image/${fname}.png
