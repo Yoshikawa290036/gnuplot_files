a35a03646286beb474ec1382c8976701424edb1d
Yoshikawa290036 <290036hirokix@gmail.com>
2022-09-16 15:54:54 +0900

    [Add] vel

---
+++ vel.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+fname=vel
+
+gnuplot -e "fname = '${fname}'"  plt/vel.plt
+
+platex ${fname}.tex
+dvips ${fname}.dvi -o ${fname}.eps
+
+convert -density 600 -units PixelsPerInch -alpha off ${fname}.eps ${fname}.png
+
+rm ${fname}.aux
+rm ${fname}.dvi
+rm ${fname}.eps
+rm ${fname}.log
+rm ${fname}.tex
+rm ${fname}-inc.eps
+
+# convert ${fname}.png -trim ${fname}.png
+
+mv ${fname}.png  image/${fname}.png
+# mv ${fname}.eps  image/${fname}.eps
